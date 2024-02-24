150e0fa62b01687636a962bddf640b5f9262ef88
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-24 19:44:45 +0530

    Api Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("Delhi");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+    import.meta.env.VITE_API_KEY
+  }&units=metric`;
+
+  const getWeather = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [city]);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
