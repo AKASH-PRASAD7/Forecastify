c1c2f3a8225e09b5d97269128500b2aaf8881700
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-25 14:45:51 +0530

    Search done

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,39 @@ function App() {
   const [data, setData] = useState(null);
   const [city, setCity] = useState("Delhi");
   const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
     import.meta.env.VITE_API_KEY
   }&units=metric`;
 
-  const getWeather = useCallback(async () => {
+  const getWeather = async () => {
     try {
       const res = await fetch(url);
       const dataObj = await res.json();
-      setData(dataObj);
-    } catch (error) {
-      console.error("Error fetching weather:", error);
-      // Handle error gracefully, e.g., display an error message to the user
+      if (dataObj.cod !== 200) {
+        setError("City not found");
+      }
+      if (dataObj.cod === 200) {
+        setData(dataObj);
+        setError("");
+      }
+    } catch (err) {
+      setError(err.message);
     }
-  }, [url]); // Only re-create getWeather if url changes
-
-  const memoizedWeatherData = useMemo(() => getWeather(), [getWeather]);
-
-  const handleSearch = (searchString) => {
-    setSearch(searchString);
-    // setCity(searchString);
-    console.log(city);
   };
 
-  // useEffect(() => {
-  //   getWeather();
-  // }, []);
+  useEffect(() => {
+    getWeather();
+  }, [city]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setCity(search);
+      setSearch("");
+    }
+  };
+
   return (
     <>
       <main>
@@ -43,22 +49,31 @@ function App() {
           <input
             type="text"
             value={search}
-            onChange={(e) => handleSearch(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleKeyDown}
             className="outline-none mx-auto  bg-transparent glassCard rounded-xl p-2 font-semibold text-indigo-500 m-4"
             placeholder="Enter your city name"
           />
-          {memoizedWeatherData && (
-            <WeatherCard
-              place={data?.name}
-              weather={data?.weather[0]?.main}
-              icon={data?.weather[0]?.icon}
-              temp={data?.main.temp}
-              temp_min={data?.main.temp_min}
-              temp_max={data?.main.temp_max}
-              feels_like={data?.main.feels_like}
-              humidity={data?.main.humidity}
-              wind={data?.wind.speed}
-            />
+          {error ? (
+            <>
+              <p className="text-2xl font-semibold text-center m-4 p-2 text-red-600 bg-red-300 rounded-xl">
+                ⚠️ {error}
+              </p>
+            </>
+          ) : (
+            data && (
+              <WeatherCard
+                place={data?.name}
+                weather={data?.weather[0]?.main}
+                icon={data?.weather[0]?.icon}
+                temp={data?.main.temp}
+                temp_min={data?.main.temp_min}
+                temp_max={data?.main.temp_max}
+                feels_like={data?.main.feels_like}
+                humidity={data?.main.humidity}
+                wind={data?.wind.speed}
+              />
+            )
           )}
         </section>
       </main>
